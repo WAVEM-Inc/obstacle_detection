6ac5b4ef169ba8827d1aab4895a513b8fd7c7eac
felicita64 <felicita64@gmail.com>
2024-03-05 17:15:15 +0900

    add sub area

--- obstacle_detection/include/obstacle_detection/obstacle_detection.hpp
+++ obstacle_detection/include/obstacle_detection/obstacle_detection.hpp
@@ -3,18 +3,27 @@
 
 #define DETECT_SIZE 10
 #define DETECT_RES 20
+#define CAR_WIDTH 1.2
+#define SCAN_FILTER_DIST 0.2
+#define OBS_MOVE_DIST 3
 
 #include <iostream>
 #include <fstream>
 #include <fcntl.h>
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "obstacle_msgs/msg/area.hpp"
 #include <unistd.h>
-using LidarMSG = sensor_msgs::msg::LaserScan ;
+using LidarMSG = sensor_msgs::msg::LaserScan;
+using AreaMSG = obstacle_msgs::msg::Area;
 class ObsDetection : public rclcpp::Node{
     private :
         rclcpp::Subscription<LidarMSG>::SharedPtr sub_scan_;
+        rclcpp::Subscription<AreaMSG>::SharedPtr sub_area_;
         void scan_callback(const std::shared_ptr<LidarMSG> scan);
+	void area_callback(const std::shared_ptr<AreaMSG> area);
+	float area_x1, area_x2, area_y1, area_y2;
+	int area_status;
     public :
         ObsDetection();
 	
--- obstacle_detection/src/obstacle_detection.cpp
+++ obstacle_detection/src/obstacle_detection.cpp
@@ -1,30 +1,67 @@
 #include"obstacle_detection.hpp"
 
 ObsDetection::ObsDetection():Node("obstacle_detection_node"){
-	sub_scan_ = this->create_subscription<LidarMSG>("/scan", 1, std::bind(&ObsDetection::scan_callback ,this ,std::placeholders::_1));
+	sub_scan_ = this->create_subscription<LidarMSG>("/sensor/scan_merge", 1, std::bind(&ObsDetection::scan_callback ,this ,std::placeholders::_1));
+	sub_area_ = this->create_subscription<AreaMSG>("/drive/obstacle/area", 1, std::bind(&ObsDetection::area_callback ,this ,std::placeholders::_1));
+}
+
+void ObsDetection::area_callback(const std::shared_ptr<AreaMSG> area)
+{
+
 }
 
 void ObsDetection::scan_callback(const std::shared_ptr<LidarMSG> scan){
 	int robot_detect_x,robot_detect_y;
 	int lp,lp_x,lp_y;
-	float area_size_x1, area_size_x2, area_size_y1, area_size_y2;
 
 	////tmp value
 	float distance_x,lidar_range;
 	int distance_val,detect_val, detect_flag;
+	float vel_x;
+	switch(area_status)
+	{
+		case 0:
+			area_x1=-CAR_WIDTH/2;
+			area_x2=CAR_WIDTH/2;
+			if(vel_x >= 0)
+			{
+				area_y1=-OBS_MOVE_DIST;
+				area_y2=0;
+			}
+			else
+			{
+				area_y1=0;
+				area_y2=OBS_MOVE_DIST;
+			}
+			break;	
+		case 1:
+			area_x1=0;
+			area_y1=0;
+			area_x2=0;
+			area_y2=0;
+			break;	
+		case 2:
+			area_x1=0;
+			area_y1=0;
+			area_x2=0;
+			area_y2=0;
+			break;	
+		default:
+			
+	}
 	if(distance_x < 0)
 	{
-		area_size_x1=-0.27;
-		area_size_x2=0.27;
-		area_size_y1=0.0;
-		area_size_y2=lidar_range - 0.0;
+		area_x1=-0.27;
+		area_x2=0.27;
+		area_y1=0.0;
+		area_y2=lidar_range - 0.0;
 	}
 	else
 	{
-		area_size_x1=-0.27;
-		area_size_x2=0.27;
-		area_size_y1=-lidar_range+0.0;
-		area_size_y2=0.0;
+		area_x1=-0.27;
+		area_x2=0.27;
+		area_y1=-lidar_range+0.0;
+		area_y2=0.0;
 	}
 	int num_ranges = scan->ranges.size();
 
@@ -34,7 +71,7 @@ void ObsDetection::scan_callback(const std::shared_ptr<LidarMSG> scan){
 	float occ_x,occ_y;
 	for(lp=1;lp<scan->ranges.size();lp++)
 	{
-		if((!std::isnan(scan->ranges[lp])) && (scan->ranges[lp] > 0.019 && ((scan->ranges[lp]-scan->ranges[lp-1]) < 0.1 )))
+		if((!std::isnan(scan->ranges[lp])) && (scan->ranges[lp] > 0.019 && ((scan->ranges[lp]-scan->ranges[lp-1]) < SCAN_FILTER_DIST )))
 		{
 			occ_x=-scan->ranges[lp]*sin( (scan->angle_min+scan->angle_increment*lp) );
 			occ_y=-scan->ranges[lp]*cos( (scan->angle_min+scan->angle_increment*lp) );
@@ -45,11 +82,11 @@ void ObsDetection::scan_callback(const std::shared_ptr<LidarMSG> scan){
 		}
 	}
 
-	robot_detect_x=(DETECT_SIZE + (float)(area_size_x1))*DETECT_RES;
-	robot_detect_y=(DETECT_SIZE + (float)(area_size_y1))*DETECT_RES;
-	for(lp_y=0;lp_y < DETECT_RES*(area_size_y2-area_size_y1);lp_y++)
+	robot_detect_x=(DETECT_SIZE + (float)(area_x1))*DETECT_RES;
+	robot_detect_y=(DETECT_SIZE + (float)(area_y1))*DETECT_RES;
+	for(lp_y=0;lp_y < DETECT_RES*(area_y2-area_y1);lp_y++)
 	{
-		for(lp_x=0;lp_x < DETECT_RES*(area_size_x2-area_size_x1);lp_x++)
+		for(lp_x=0;lp_x < DETECT_RES*(area_x2-area_x1);lp_x++)
 		{
 			if(detect_area[robot_detect_x+lp_x][robot_detect_y+lp_y] == 1)
 			{
