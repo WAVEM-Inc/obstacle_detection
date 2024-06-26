f1267f030cb6b480a4e23699cc963b1d23673776
nuc-bt <felicita64@gmail.com>
2024-06-26 16:31:03 +0900

    feat : lidar 영역 감지 좌표 획득 로직 수정중

--- test/obs_node_pub.sh
+++ test/obs_node_pub.sh
@@ -4,8 +4,8 @@ speaker: 0
 start_node:
   node_id: '201'
   position:
-    latitude: 37.3060172327693
-    longitude: 127.24018700061171
+    latitude: 36.11361380000634
+    longitude: 128.368702800012
   type: 'work_node'
   kind: 'endpoint'
   heading: 148.0
@@ -15,12 +15,12 @@ start_node:
 end_node:
   node_id: '202'
   position:
-    latitude: 37.3060473
-    longitude: 127.2402091
+    latitude: 36.113596
+    longitude: 128.368733
   type: ''
   kind: 'waiting'
   heading: 148.0
   direction: ''
   driving_option: ''
-  detection_range: [{offset: 0.5, width_left: 5.0, width_right: 5.0, height: 6.0, action_code: waiting}]" 
+  detection_range: [{offset: 1.0, width_left: 0.5, width_right: 0.5, height: 5.0, action_code: waiting}]" 
 
